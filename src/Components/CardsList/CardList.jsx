c1d7d13925bd0ab996843f290ac4c48bccaf159e
daniil <shkolnikov.daniil25@gmail.com>
2020-08-04 21:59:32 +0300

    Card markup

--- src/Components/CardsList/CardList.jsx
+++ src/Components/CardsList/CardList.jsx
@@ -6,8 +6,14 @@ export default class CardList extends React.Component {
     render() {
         return (
             <Row>
-                <Col md className={classes.Test}></Col>
-                <Col md className={classes.Test}></Col>
+                <Col md className={classes.Test}>
+                    <Row>
+                        <Col md style={{ background: 'yellow', height: '8vh' }}>ТЕГИ</Col>
+                    </Row>
+                    <Row>
+                        <Col md style={{ background: 'rgb(65, 74, 78)', height: '86.5vh', color: 'white' }}>Карточки</Col>
+                    </Row>
+                </Col>
             </Row>
         )
     }
