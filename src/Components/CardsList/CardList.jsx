8d820e05f23ea10dd0a64ab1b8dc7d487357bd57
daniil <shkolnikov.daniil25@gmail.com>
2020-08-04 22:52:16 +0300

    CardList cards-center

--- src/Components/CardsList/CardList.jsx
+++ src/Components/CardsList/CardList.jsx
@@ -1,17 +1,76 @@
 import React, { Component } from 'react'
 import classes from './CardList.module.css';
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Card } from 'react-bootstrap'
+import CardListContainer from './CardListContainer';
 
 export default class CardList extends React.Component {
     render() {
         return (
             <Row>
-                <Col md className={classes.Test}>
+                <Col  className={classes.Test}>
                     <Row>
                         <Col md style={{ background: 'yellow', height: '8vh' }}>ТЕГИ</Col>
                     </Row>
                     <Row>
-                        <Col md style={{ background: 'rgb(65, 74, 78)', height: '86.5vh', color: 'white' }}>Карточки</Col>
+                        <Col md={12}  style={{ background: 'rgb(65, 74, 78)', height: '86.5vh', color: 'white' }}>
+                            Карточки
+                                <Row className="justify-content-center">
+                                <Card className="ml-3 mr-3" style={{ width: '18rem', color: 'black' }}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Title
+                                    </Card.Title>
+                                        <Card.Text>
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem adipisci veritatis necessitatibus, voluptas dolorum natus repudiandae autem minus perferendis et, consequuntur iure, saepe earum vitae! Quae iure odio possimus?
+                                    </Card.Text>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                    </Card.Body>
+                                </Card>
+
+
+                                <Card className="ml-3 mr-3" style={{ width: '18rem', color: 'black' }}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Title
+                                    </Card.Title>
+                                        <Card.Text>
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem adipisci veritatis necessitatibus, voluptas dolorum natus repudiandae autem minus perferendis et, consequuntur iure, saepe earum vitae! Quae iure odio possimus?
+                                    </Card.Text>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                    </Card.Body>
+                                </Card>
+
+
+                                <Card className="ml-3 mr-3" style={{ width: '18rem', color: 'black' }}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Title
+                                    </Card.Title>
+                                        <Card.Text>
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem adipisci veritatis necessitatibus, voluptas dolorum natus repudiandae autem minus perferendis et, consequuntur iure, saepe earum vitae! Quae iure odio possimus?
+                                    </Card.Text>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                    </Card.Body>
+                                </Card>
+
+                                <Card className="ml-3 mr-3" style={{ width: '18rem', color: 'black' }}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Title
+                                    </Card.Title>
+                                        <Card.Text>
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatem adipisci veritatis necessitatibus, voluptas dolorum natus repudiandae autem minus perferendis et, consequuntur iure, saepe earum vitae! Quae iure odio possimus?
+                                    </Card.Text>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                        <Card.Link href='#'>Link</Card.Link>
+                                    </Card.Body>
+                                </Card>
+
+                            </Row>
+                        </Col>
                     </Row>
                 </Col>
             </Row>
